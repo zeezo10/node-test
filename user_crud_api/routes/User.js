6cb6ebd153ea40aadd96f5ad8a4cbd140f583730
aziz <abdulazizjambi98@gmail.com>
2025-01-22 16:45:26 +0700

    make update user by id endpoint

--- user_crud_api/routes/User.js
+++ user_crud_api/routes/User.js
@@ -1,34 +1,71 @@
-const express = require('express');
-const { ObjectId } = require('mongodb');
-const collection = require('../../config/mongodb').db.collection('Users');
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const collection = require("../../config/mongodb").db.collection("Users");
+const Controller = require("../controllers/Users.js");
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {
-    try {
-      const users = await collection.find().toArray();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+router.get("/users", async (req, res) => {
+  try {
+    const users = await collection.find().toArray();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-  router.post('/users', async (req, res) => {
-    const { name, email, age } = req.body;
-  
-    if (!name || !email || typeof age !== 'number') {
-      return res.status(400).json({ message: 'Invalid input data' });
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await collection.findOne({ _id: new ObjectId(req.params.id) });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  
-    try {
-      const newUser = { name, email, age };
-      const result = await collection.insertOne(newUser);
-      res.status(201).json(result.ops[0]);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/users", async (req, res) => {
+  const { name, email, age } = req.body;
+
+  if (!name || !email || typeof age !== "number") {
+    return res.status(400).json({ message: "Invalid input data" });
+  }
+
+  try {
+    const newUser = { name, email, age };
+
+    const result = await collection.insertOne(newUser);
+    res.status(201).json(result.ops[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put("/users/:id", async (req, res) => {
+  const { name, email, age } = req.body;
+
+  if (!name || !email || typeof age !== "number") {
+    return res.status(400).json({ message: "Invalid input data" });
+  }
+
+  try {
+    const updatedUser = { name, email, age };
+    const result = await collection.findOneAndUpdate(
+      { _id: new ObjectId(req.params.id) },
+      { $set: updatedUser },
+      { returnOriginal: false }
+    );
+
+    if (!result) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
-  
-  
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
